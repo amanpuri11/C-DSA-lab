2ae1c766db3183b3d8cb14c9eb8fd022771f3838
amanpuri11 <puriaman177@gmail.com>
2024-04-04 11:18:00 +0530

    deleted:    2D.c 	deleted:    ADT_switch_array.c 	deleted:    Add 2 matrix.c 	deleted:    Calculator using switch case.c 	deleted:    Interview_ques.c 	deleted:    Memory_allocation.c 	deleted:    Mult_2_matrix.c 	deleted:    Octal_to_decimal.c 	deleted:    Storage_Classes/Extern.c 	deleted:    Storage_Classes/Register.c 	deleted:    Storage_Classes/Static.c 	deleted:    Storage_Classes/auto.c 	deleted:    Storage_Classes/tempCodeRunnerFile.c 	deleted:    Structures/Array_of_structure.c 	deleted:    Structures/Strc.c 	deleted:    Structures/Union.c 	deleted:    Structures/ques2.c 	deleted:    Structures/question1.c 	deleted:    Structures/tempCodeRunnerFile.c 	deleted:    Weekdays.c 	deleted:    call by value in arr.c 	deleted:    swap.c 	deleted:    tempCodeRunnerFile.c

--- Interview_ques.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main (){
-  int i=17, j=9, k=4, l=32, m=12;
-  while(k)
-  {
-    m= -i+j++ - k-- + ++l;
-  }
-  printf("%d %d %d %d %d",i+m, j, k, l, m);
-}
--- Storage_Classes/Extern.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-void main(){
-    extern int a;
-    printf("%d",a);
-}
-int a=10;   // output: 10
-
